462012ce3c5d498397f07f661c8e20355c564c59
Baptibest <baratchartb31@gmail.com>
2026-03-05 10:44:04 +0100

    Increase Spendly logo size in navbar

--- components/layout/PublicNavbar.tsx
+++ components/layout/PublicNavbar.tsx
@@ -23,7 +23,7 @@ export default function PublicNavbar() {
               <img
                 src="/Spendly.png"
                 alt="Spendly"
-                className="h-10 w-auto"
+                className="h-16 w-auto"
               />
             </Link>
           </div>
