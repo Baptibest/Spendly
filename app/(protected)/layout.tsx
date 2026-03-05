40b645aeb0183c952e2619b8866936c2532632f9
Baptibest <baratchartb31@gmail.com>
2026-03-05 22:28:28 +0100

    Ajouter fond bleu clair discret à l'application

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -23,11 +23,11 @@ export default function ProtectedLayout({
   }, [router, pathname]);
 
   return (
-    <>
+    <div className="min-h-screen bg-blue-50">
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {children}
       </main>
-    </>
+    </div>
   );
 }
